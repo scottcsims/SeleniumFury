2ca460d58eeb87ab7e7d1460dee2e301eb88ca0f
ssims <ssims98@gmail.com>
2010-11-04 08:08:39 -0500

    Directing to a public site. Adding License.

--- spec/page_generator_spec.rb
+++ spec/page_generator_spec.rb
@@ -4,13 +4,26 @@ describe PageGenerator do
   include CreateBrowserDriver
   include PageGenerator
   append_after(:each) do
-    browser.close_current_browser_session
+    if @browser
+      browser.close_current_browser_session
+    end
   end
-  it "should get source and print elements" do
-    create_selenium_driver("http://stage.homeaway.com/")
+
+  it "should create a nokogiri doc" do
+    doc = <<EOF
+     <a href="/index/1">First</a>
+EOF
+    @parser = Nokogiri.HTML doc
+    @parser.css("a").each do |html_element|
+      puts html_element
+    end
+  end
+  it "should find elements on ebay advanced search" do
+# http://computers.shop.ebay.com/ebayadvsearch/
+    create_selenium_driver("http://computers.shop.ebay.com")
     browser.start_new_browser_session
     puts "Testing #{browser.browser_url} on #{browser.browser_string} "
-    browser.open "/254680"    
+    browser.open "/ebayadvsearch"
     get_source_and_print_elements(browser)
   end
 end
--- spec/page_validator_spec.rb
+++ spec/page_validator_spec.rb
@@ -1,6 +1,6 @@
 require File.dirname(__FILE__) + "/spec_helper"
 require File.dirname(__FILE__) + "/../lib/page_validator"
-require File.dirname(__FILE__) + "/property_page"
+require File.dirname(__FILE__) + "/advanced_search"
 describe PageValidator do
   include CreateBrowserDriver
   include PageValidator
@@ -8,9 +8,9 @@ describe PageValidator do
     browser.close_current_browser_session
   end
   it "should validate the elements contained on the page file" do
-    create_selenium_driver("http://stage.homeaway.com/")
+    create_selenium_driver("http://computers.shop.ebay.com")
     browser.start_new_browser_session
     puts "Testing #{browser.browser_url} on #{browser.browser_string} "
-    check_page_file_class(PropertyPage, "/254680")
+    check_page_file_class(AdvancedSearch, "/ebayadvsearch")
   end
 end
