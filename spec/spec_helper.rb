000daf4e96fe4c08ee5a5cc018335500b89ae238
ssims <ssims98@gmail.com>
2011-03-31 08:44:54 -0500

    Deleting specs

--- spec/spec_helper.rb
+++
@@ -1,7 +0,0 @@
-require 'rubygems'
-gem "rspec", "=1.1.12"
-require "spec"
-gem "selenium-client", ">=1.2.18"
-require "selenium/client"
-require File.dirname(__FILE__) + "/create_browser_driver"
-require 'nokogiri'
