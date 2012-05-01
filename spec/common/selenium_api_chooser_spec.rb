f19f315557124fc21806d72ed7400d2053dfdae9
Scott Sims <ssims@homeaway.com>
2012-05-01 14:48:01 -0500

    updating naming

--- spec/common/selenium_api_chooser_spec.rb
+++ spec/common/selenium_api_chooser_spec.rb
@@ -23,13 +23,13 @@ describe SeleniumFury::SeleniumApiChooser do
     end
   end
   context "Integrating with generator/validator methods" do
-    it "should find generate elements for selenium client tests" do
+    it "should find generate method for selenium client tests" do
       create_selenium_driver "http://www.scottcsims.com"
       browser.start_new_browser_session
       browser.open "/"
-      generate(browser).should include("found (8 elements)")
+      generate(browser).should include("found (9 elements)")
     end
-    it "should find validate elements for selenium client tests" do
+    it "should find validate method for selenium client tests" do
       create_selenium_driver("http://www.homeaway.com")
       browser.start_new_browser_session
       puts "Testing #{browser.browser_url} on #{browser.browser_string} "
