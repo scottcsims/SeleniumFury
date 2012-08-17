95244e656192155bab1c0bfd3d165941f6ec073d
Alan Scherger <ascherger@homeaway.com>
2012-08-17 08:56:03 -0500

    made our features not reliant on our rspecs

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,6 +2,14 @@ $:.unshift(File.dirname(__FILE__) + '/../../lib')
 $:.unshift(File.dirname(__FILE__))
 require 'rubygems'
 require 'bundler/setup'
-require 'selenium_fury'
+
+require File.expand_path(File.dirname(__FILE__) + "/../../lib/selenium_fury")
+
 require 'rspec'
-require 'spec_helper'
+
+require File.expand_path(File.dirname(__FILE__) + "/../../spec/test_page/test_page_rc")
+require File.expand_path(File.dirname(__FILE__) + "/../../spec/test_page/test_page_custom_generator_configuration")
+
+TEST_PAGE_URL="file:///"+ File.expand_path(File.dirname(__FILE__) + "/../../spec/test_page/test_page.html")
+include SeleniumFury::SeleniumWebDriver::CreateSeleniumWebDriver
+include SeleniumFury::SeleniumClient::CreateSeleniumClientDriver
