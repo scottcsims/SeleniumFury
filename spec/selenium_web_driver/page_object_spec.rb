136830b883e45973591c3123c3fd3fceacc4dbcc
Scott Sims <ssims@homeaway.com>
2012-11-09 13:37:10 -0600

    updating examples and removing custom generator features

--- spec/selenium_web_driver/page_object_spec.rb
+++ spec/selenium_web_driver/page_object_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 describe SeleniumFury::SeleniumWebDriver::CreateSeleniumWebDriver do
-  it "should go to page using launch_web_driver" do
+  it "should go to a page using launch_web_driver" do
     launch_web_driver TEST_PAGE_URL
     driver.current_url.should include "/spec/test_page/test_page.html"
     driver.title.should == "SeleniumFury Test Page"
@@ -9,13 +9,13 @@ end
 
 describe PageObject do
 
-  it "should have a text area method on the test page" do
+  specify "has Selenium::WebDriver::Elements defined with the element method" do
     launch_web_driver TEST_PAGE_URL
     test_page = TestPage.new(driver)
     test_page.should_not be_nil
     test_page.textarea.should be_a(Selenium::WebDriver::Element)
   end
-  it "should have a test page" do
+  specify "allows a page object class can be linked to another page object class using the page method for sub pages" do
     class ComponentPage < PageObject
 
     end
@@ -23,12 +23,12 @@ describe PageObject do
     TestPage.new().component_page.should be_a(ComponentPage)
   end
 
-  it "should have an error if a non page object class is passed to page method" do
+  it "should have an error if a non page object class is passed to the page method" do
     TestPage.page(:not_a_page, String)
     expect { TestPage.new().not_a_page }.to(raise_exception(RuntimeError, "String does not inherit from PageObject"))
   end
 
-  it "should use elements on the TestPage" do
+  it "should call methods on Selenium::WebDriver::Elements defined in the TestPage class" do
     launch_web_driver TEST_PAGE_URL
     test_page = TestPage.new(driver)
     test_page.input_message.send_keys "this is awesome"
@@ -36,49 +36,19 @@ describe PageObject do
     driver.find_element(:id => "message").text.should == "this is awesome"
   end
 
-  it "should use some methods man" do
+  specify "PageObject provides a method to return all Selenium::WebDriver::Element defined in the class" do
+    Object.instance_eval { remove_const :TestPage } if Object.const_defined? :TestPage
     launch_web_driver TEST_PAGE_URL
-    test_page = TestPage.new(driver)
-    test_page.click_check_box
-    driver.find_element(:id => "input_checkbox").selected?.should be_true
-  end
-end
-  # OK, so here's some great info about the "magic" behind ruby.
-  # When ruby spins up, an Object class is created, and everything runs "inside" it
-  # When you do code like this: TestPage.new() an instance of TestPage is added as a constant
-  # to the instance of this "global" Object class
-  # Therefore, since rspec is running "inside" this Object Class, an instance of TestPage is
-  # instantiated and shared from test to test, BUT when you do get_page_object, an instance
-  # of the page object must be created and added as a constant, but if it already exists,
-  # you're gonna have bad time. Therefore, for testing sake, we must check for this,
-  # and handle it appropriately.
-  #
-  # It should also be noted that get_page_object only adds page elements to the class
-  # and currently doesn't look for a pre-defined page object in the project
-
-describe "PageObjectElements" do
-  before(:each) do
-     Object.instance_eval { remove_const :TestPage } if Object.const_defined? :TestPage
-   end
-   after(:each) do
-     Object.instance_eval { remove_const :TestPage }
-   end
-  it "should have elements" do
-    launch_web_driver TEST_PAGE_URL
+    #test_page = TestPage.new(driver)
     test_page = get_page_object(driver, 'TestPage')
     test_page.class.elements.should_not be_nil
     test_page.class.elements.should have(15).elements
     test_page.method(test_page.class.elements[0]).call.class.should == Selenium::WebDriver::Element
   end
-  it "should return a web_driver element when an attribute is accessed" do
-    launch_web_driver TEST_PAGE_URL
-    test_page = get_page_object(driver, 'TestPage')
-    test_page.textarea.class.should == Selenium::WebDriver::Element
-  end
 
   it "should raise an exception when the element can't be found'" do
     launch_web_driver TEST_PAGE_URL
-    test_page = get_page_object(driver, 'TestPage')
+    test_page = TestPage.new(driver)
     TestPage.element :not_a_element, {:id => "not a element"}
     expect { test_page.not_a_element }.to raise_exception RuntimeError, "Could not find element not_a_element"
   end
