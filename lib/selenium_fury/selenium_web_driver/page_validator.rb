1a4f01e80e3b0cd281b13e1fff36dc921feb3948
Titus Fortner <tfortner@homeaway.com>
2013-09-23 17:13:02 -0500

    Nokogiri should be the default for validation

--- lib/selenium_fury/selenium_web_driver/page_validator.rb
+++ lib/selenium_fury/selenium_web_driver/page_validator.rb
@@ -23,7 +23,7 @@ module SeleniumFury
         puts "class #{page_class}"
         page_object=page_class.new(driver)
         raise "Could not find web driver elements in #{page_class}" if page_class.elements.nil?
-        page_source = Nokogiri::HTML(@driver.page_source) if validate_tags.fetch(:verification_type, nil) == :nokogiri
+        page_source = Nokogiri::HTML(@driver.page_source)
         page_class.elements.each do |web_driver_element_name|
           puts "\tValidating #{web_driver_element_name}"
           begin
@@ -46,13 +46,18 @@ module SeleniumFury
                                  true
                              end
           if validate_element
-            # Note - Using Nokogiri type requires valid CSS and no periods present in ID attributes to work
-            if validate_tags.fetch(:verification_type, nil) == :nokogiri
-              css_value = element_obj.location.invert.keys.first
-              element_obj.location.keys.first == :id && css_value = "##{css_value.gsub('.', '\.')}"
+            if element_obj.location.keys.first != :id && element_obj.location.keys.first != :css
+              element_present = element_obj.present?
+              # Per spec, IDs should not have periods, but some do
+              # Theoretically Nokogiri should be able to escape them, but it can't
+            elsif element_obj.location.keys.first == :id && element_obj.location[:id].include?('.')
+              element_present = element_obj.present?
+            elsif element_obj.location.keys.first == :id
+              css_value = "##{element_obj.location.invert.keys.first}"
               element_present = page_source.css(css_value).size >= 1
             else
-              element_present = element_obj.present?
+              css_value = element_obj.location.invert.keys.first
+              element_present = page_source.css(css_value).size >= 1
             end
             unless element_present
               puts "\t\t\tCould not find #{web_driver_element_name}"
